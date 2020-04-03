3e8182c1c368397ce082c4fa3ee1bd6df2d026ae
Benjamin Smith <smithbm2316@gmail.com>
2020-04-03 01:34:11 -0700

    Added highlight.js support, preliminary css

---
+++ src/js/highlight-styles.js
@@ -0,0 +1,6 @@
+import hljs from "highlight.js";
+import javascript from "highlight.js/lib/languages/javascript";
+import "highlight.js/styles/night-owl.css";
+hljs.registerLanguage("javascript", javascript);
+
+hljs.initHighlightingOnLoad();
---
+++ src/js/test.js
@@ -0,0 +1,10 @@
+// This is a comment
+const names = [
+	"Ben Smith",
+	"John Michaelson",
+	"Wes Tomkins",
+	"Julie Donaldson",
+	"Katelyn Roberts",
+];
+
+const firstNames = names.map(name => name.split(" ")[0]);
