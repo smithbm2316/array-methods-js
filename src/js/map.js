089157f485d94644ca0a3a556c2c339dd5bd99ff
Benjamin Smith <smithbm2316@gmail.com>
2020-04-04 01:01:22 -0700

    updated css

--- src/js/map.js
+++ src/js/map.js
@@ -14,12 +14,7 @@ const exampleTwo = document.querySelector(".example-two");
 const text = `\
 const names = ["Ben Smith", "John Michaelson", "Wes Tomkins", "Julie Donaldson", "Katelyn Roberts"];
 const firstNames = names.map(name => name.split(" ")[0]);
-function func() {
-  
-}
 `;
-
-function func() {}
 
 exampleOne.textContent = text;
 exampleTwo.textContent = text;
