700cc51585981e6b76a97ebf842f0fbe6259a2ba
Benjamin Smith <smithbm2316@gmail.com>
2020-04-04 23:54:52 -0700

    added theme toggling, nord theme colors as css vars, removed excess files

---
+++ src/js/index.js
@@ -0,0 +1,40 @@
+import ClipboardJS from "clipboard";
+import Prism from "prismjs";
+import NormalizeWhitespace from "prismjs/plugins/normalize-whitespace/prism-normalize-whitespace";
+
+/* ClipboardJS script */
+// Grab all the copy buttons on the various snippets
+const examples = document.querySelectorAll(".snippet-copy");
+const clipboard = new ClipboardJS(examples);
+
+/* PrismJS script */
+Prism.highlightAll();
+
+/* Light/dark mode theme toggle */
+// On page load, check user's color scheme preference and set default scheme
+window.onload = (event) => {
+	// Define the query to check
+	const schemeQuery = window.matchMedia("(prefers-color-scheme: dark)");
+
+	// Set scheme based upon whether user's preferences is dark or light
+	schemeQuery.matches
+		? document.documentElement.setAttribute("data-theme", "dark")
+		: document.documentElement.setAttribute("data-theme", "light");
+};
+
+// Select the theme toggle button
+const toggleThemeBtn = document.querySelector(".toggle-theme");
+
+// Add an event listener that adds the correct theme via data-theme attribute
+toggleThemeBtn.addEventListener("click", () => {
+	// Add class for transition only when toggling the theme
+	document.documentElement.classList.add("theme-transition");
+
+	// Toggle the theme
+	document.documentElement.getAttribute("data-theme") === "dark"
+		? document.documentElement.setAttribute("data-theme", "light")
+		: document.documentElement.setAttribute("data-theme", "dark");
+
+	// Remove the transition class, so it doesn't show it on page reload
+	document.documentElement.classList.remove(".theme-transition");
+});
